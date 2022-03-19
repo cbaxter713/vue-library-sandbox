de571adbbfe47b33599ddc309dc1179c71387a5c
Colin Baxter <colin.baxter@zaneray.com>
2022-03-19 15:01:01 -0600

    Adding publishing config setup

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Base Components
+export { default as Button } from './components/base/Button/Button.vue';
+
+// Pattern Components
+export { default as SplitCallout } from './components/patterns/SplitCallout/SplitCallout.vue';
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,41 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-import VueTypeImports from 'vite-plugin-vue-type-imports'
+const path = require('path');
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import VueTypeImports from 'vite-plugin-vue-type-imports';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),     
-    VueTypeImports(),
-  ],
+  plugins: [vue(), VueTypeImports()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-    },
+      '@': resolve(__dirname, 'src')
+    }
   },
-
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `@import "@/styles/imports.scss";`
-      },
+      }
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'MyComponentLib',
+      fileName: (format) => `my-component-lib.${format}.js`
     },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
   }
-})
+});
