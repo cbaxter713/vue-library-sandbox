54a6cc23788ea7cd842fb9148b80c73e4abe2c84
Colin Baxter <colin.baxter@zaneray.com>
2022-03-19 16:01:04 -0600

    Trying some changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import Button from './components/base/Button/Button.vue';
+import HelloWorld from './components/HelloWorld.vue';
+// import Button from './components/base/Button/Button.vue';
+import { Button } from '@cbaxterpackages/vue-library-sandbox';
 </script>
 
 <template>
   <HelloWorld first-name="Jenn" last-name="Baxter" />
-  <Button label="First Button" size="md" theme="primary" :loading="false" />
+  <!-- <Button label="First Buttonsss" size="md" theme="primary" :loading="false" />
   <Button label="Second Button" theme="secondary" />
-  <Button theme="primary" size="sm" label="Colin" />
+  <Button theme="primary" size="sm" label="Colin" /> -->
+  <Button label="Test Button" />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import VueTypeImports from 'vite-plugin-vue-type-imports';
+import injectCss from '@cxing/vitejs-plugin-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueTypeImports()],
+  plugins: [vue(), VueTypeImports(), injectCss()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
