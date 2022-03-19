ff2a32cb05dfbceb8143f805036f7cbe4f373ae9
Colin Baxter <colin.baxter@zaneray.com>
2022-03-19 11:09:47 -0600

    UPdating logo

--- .storybook/customThemeName.js
+++ .storybook/customThemeName.js
@@ -1,41 +1,42 @@
-import { create } from "@storybook/theming";
+import { create } from '@storybook/theming';
+
+import customLogo from '../public/images/custom-logo.png';
 
 export default create({
-  brandTitle: "Colins custom storybook",
-  brandUrl: "https://espn.com",
+  brandTitle: 'Colins custom storybook',
+  brandUrl: 'https://espn.com',
   //brandImage: "https://place-hold.it/350x150",
-  brandImage:
-    "https://searchvectorlogo.com/wp-content/uploads/2020/01/onx-onxmaps-logo-vector.png",
+  brandImage: customLogo,
 
-  base: "light",
+  base: 'light',
   //base: "dark",
 
-  colorPrimary: "black",
-  colorSecondary: "red",
+  colorPrimary: 'black',
+  colorSecondary: 'red',
 
   // UI
-  appBg: "#f5f5f5",
-  appContentBg: "#fff",
-  appBorderColor: "grey",
+  appBg: '#f5f5f5',
+  appContentBg: '#fff',
+  appBorderColor: 'grey',
   appBorderRadius: 4,
 
   // Typography
-  fontBase: "Roboto, Helvetica, Arial, sans-serif",
-  fontCode: "monospace",
+  fontBase: 'Roboto, Helvetica, Arial, sans-serif',
+  fontCode: 'monospace',
 
   // Text colors
-  textColor: "black",
-  textInverseColor: "rgba(255,255,255,0.9)",
-  textMutedColor: "#464646",
+  textColor: 'black',
+  textInverseColor: 'rgba(255,255,255,0.9)',
+  textMutedColor: '#464646',
 
   // Toolbar default and active colors
-  barTextColor: "black",
-  barSelectedColor: "red",
-  barBg: "#dadada",
+  barTextColor: 'black',
+  barSelectedColor: 'red',
+  barBg: '#dadada',
 
   // Form colors
-  inputBg: "white",
-  inputBorder: "silver",
-  inputTextColor: "black",
-  inputBorderRadius: 4,
+  inputBg: 'white',
+  inputBorder: 'silver',
+  inputTextColor: 'black',
+  inputBorderRadius: 4
 });
